296e1945e46de8f332a30e30cfa2b956193ef7a3
Laura Estremera <149011375+irllyliketoast@users.noreply.github.com>
2025-03-29 21:57:22 -0400

    Create Supplier.java

---
+++ CSC 331 Final Project/src/Supplier.java
@@ -0,0 +1,15 @@
+public class Supplier {
+    /**
+     * @author Group 15 - Laura Estremera
+     * Course: CSC 331-001
+     * Date: 4/5/2024
+     * Description: Define classes to represent suppliers and methods for managing
+     *  supplier information. Implement functionalities such as adding new
+     *  suppliers, updating supplier details, etc.
+     */
+}
+// subclasses will include suppliers like a florist, meat supplier, etc
+//     private int id;
+//    private String name;
+//    // Other relevant attributes
+//    // Constructor, getters, setters, etc.
